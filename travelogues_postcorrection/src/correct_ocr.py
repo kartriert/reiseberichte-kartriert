3231b303abe6e233ded64787af7002e85456183a
sarahondraszek <s1saondr@uni-trier.de>
2022-12-12 09:21:39 +0100

    add coordinates

--- travelogues_postcorrection/src/correct_ocr.py
+++ travelogues_postcorrection/src/correct_ocr.py
@@ -16,6 +16,7 @@ import re
 import os.path
 import glob
 
+
 def correct_s(txt):
     # o -> º
     output_txt = re.sub("º", "o", txt)
@@ -27,6 +28,7 @@ def correct_s(txt):
     # Replace aͤ, oͤ, uͤ with ä, ö, ü
     return output_txt
 
+
 def single_characters(txt):
     # Connect words that are separated by line breaks and connected with "-"
     txt = re.sub("(\s)([\S]+)(-\n)", r"\n\2", txt)
@@ -36,11 +38,13 @@ def single_characters(txt):
     txt = re.sub("(\w+) (-) (\w+)", r"\1\2\3", txt)
     return txt
 
+
 def delete_specials(txt):
     # Remove non-word characters / all characters except the following
     txt = re.sub("[^a-z0-9\n,üöäß.; ]", " ", txt)
     # Note: Do not delete "-" in between letters, since it can impair the NER's ability to find places (Example: Ost-Indien / Ostindien)
     return txt
+
 
 def common_mistakes(txt):
     # Correct common mistakes
@@ -67,12 +71,14 @@ def common_mistakes(txt):
     # More common country spelling mistakes...?
     return txt
 
+
 def correct_consonants(txt, textfile):
     """
-    How useful is this? One text has over 1600 found instances that must then all be corrected; however, many texts also only have 20-50
+    How useful is this? One text has over 1600 found instances that must then all be corrected; however, many texts
+    also only have 20-50
     """
     # Semi-manual correction of instances, where at least 5 consonants succeed each other
-    
+
     # Give a list of all indexes where at least 19 letters succeed each other
     letter_list = [m.start() for m in re.finditer("\w{19}\w*", txt)]
     # Save the length of the list to use as indicator later
@@ -80,9 +86,11 @@ def correct_consonants(txt, textfile):
     print(maxx)
     # Initiate list index
     index = 0
-    
+
     if letter_list != []:
-        print(textfile, "will be corrected now. To correct the shown text, put in the full, corrected text. To skip, type s. To end correction, type e. (Note: In that case, the file will still be saved normally).")
+        print(textfile,
+              "will be corrected now. To correct the shown text, put in the full, corrected text. To skip, type s. To "
+              "end correction, type e. (Note: In that case, the file will still be saved normally).")
         # Initiate new text variable, onto which the new text will be saved
         output_txt = txt[0:letter_list[0]]
         # Iterate over found instances
@@ -94,12 +102,12 @@ def correct_consonants(txt, textfile):
             # In case there is no next line break, use the length of the text, yielding the last index
             except:
                 line_break = len(txt) - item
-            print(txt[item:item+line_break])
+            print(txt[item:item + line_break])
             # Input the correction
             correction = input("Correction:")
             if correction == "s":
                 # Save the uncorrected text
-                output_txt = output_txt + txt[item:item+line_break]
+                output_txt = output_txt + txt[item:item + line_break]
             elif correction == "e":
                 # Save the uncorrected text and the rest of the text
                 output_txt = output_txt + txt[item:-1] + txt[-1]
@@ -108,46 +116,47 @@ def correct_consonants(txt, textfile):
                 # Save the correction
                 output_txt = output_txt + correction
             # Add 1 to the index
-            index+=1
+            index += 1
             # Check if the loop has one more iteration. If not, save the rest of the text
             if index == maxx:
-                output_txt = output_txt + txt[item+line_break:-1]
+                output_txt = output_txt + txt[item + line_break:-1]
                 break
             # If yes, save only up until before the next iteration
             else:
-                output_txt = output_txt + txt[item+line_break:letter_list[index]]
+                output_txt = output_txt + txt[item + line_break:letter_list[index]]
     else:
         output_txt = txt
     return output_txt
+
 
 def save_file(txt, filename):
     # Script to save the text file
     filename = filename.split('\\')[1]
     filename = os.path.join("testtexte_verbessert", filename)
-    #filename = os.path.join("18th_century_corr1", filename)
+    # filename = os.path.join("18th_century_corr1", filename)
     with open(filename, "w", encoding="utf-8") as f:
         f.write(txt)
     print(filename, "File saved!")
     return
 
+
 def main():
     for textfile in glob.glob(os.path.join("testtexte", "*.*")):
-    #for textfile in glob.glob(os.path.join("18th_century", "*.*")):
+        # for textfile in glob.glob(os.path.join("18th_century", "*.*")):
         with open(textfile, "r", encoding="utf-8") as f:
             txt = f.read().lower()
-        #print(txt)
+        # print(txt)
         txt = correct_s(txt)
-        #print(txt)
+        # print(txt)
         txt = single_characters(txt)
-        #print(txt)
+        # print(txt)
         txt = delete_specials(txt)
-        #print(txt)
+        # print(txt)
         txt = common_mistakes(txt)
-        #print(txt)
+        # print(txt)
         txt = correct_consonants(txt, textfile)
-        #print(txt)
+        # print(txt)
         save_file(txt, textfile)
 
+
 main()
-
-
