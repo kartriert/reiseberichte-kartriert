f0e42ab398b45df05c653e1764a6e54cb4f3515d
eddasof <104982063+eddasof@users.noreply.github.com>
2022-12-30 11:07:45 +0100

    Add files via upload
    Rewrite of add_info_to_JSON.ipynb for single file processing as described in travelogues/README.md

---
+++ travelogues/src/add_info_to_JSON_single_file.py
@@ -0,0 +1,20 @@
+import json
+import glob
+import os
+
+import geocoder
+
+# Enter the filename if you want to process only a single file.
+file = '../data/output/text_ner/Z103519403.json'
+with open (file, 'r') as f:
+    json_file = json.load(f)
+    for feature in json_file["features"]:
+        feature_label = feature["properties"]["source_label"]
+        # With GeoNames URL
+        if feature["properties"]["url"] == "https://www.geonames.org/None":
+            g = geocoder.geonames(feature_label, key='kartriert')
+            feature["properties"]["url"] = "https://www.geonames.org/" + str(g.geonames_id)
+
+        json_dump = json.dumps(json_file, indent=4)
+        with open('../data/output/text_ner/with_url/' + os.path.basename(file) + '_test', 'w') as f_w:
+            f_w.write(json_dump)
